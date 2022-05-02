b0b99e5ee5dbf2ac5c1408af57462f145dcbcf75
doyougnu <jeffrey.young@iohk.io>
2022-05-02 14:40:26 -0400

    add minimal flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Haskell Optimization Handbook flake";
+
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
