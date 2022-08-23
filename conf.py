52a80ef2bcea82b2116489192c3890b8a6e16525
doyougnu <jeffrey.young@iohk.io>
2022-08-23 07:00:50 -0400

    fix README orphan

--- conf.py
+++ conf.py
@@ -52,7 +52,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.dir-locals.el', '.projectile']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.dir-locals.el', '.projectile', 'README.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
