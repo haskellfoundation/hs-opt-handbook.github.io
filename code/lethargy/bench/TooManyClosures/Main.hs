abaf433ef56d572b772b0ca4aba5b7dc8332a7cf
doyougnu <jeffrey.young@iohk.io>
2022-10-01 08:17:30 -0400

    excessive closure allocation code sample

--- code/lethargy/bench/TooManyClosures/Main.hs
+++ code/lethargy/bench/TooManyClosures/Main.hs
@@ -12,34 +12,36 @@
 -- 'The Programs of Consistent Lethargy' for a discussion of this code and
 -- behavior.
 --
--- Tested with GHC 8.10.7
+-- Tested with GHC 9.2.4
 -----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -O0 -ddump-simpl -ddump-to-file -ddump-stg-final #-}
 
 module Main where
 
 import Gauge
 
-import System.Random
-import Control.DeepSeq
+-- ys :: [Int]
+-- ys = [1..10000]
 
--- | This function creates excessive closures. Once we move the infinite list
--- [0..] behind the lambda the list /will be created anew/ for each call of the
--- lambda.
--- bad :: (Int -> a) -> [Int] -> (Int -> a)
--- bad f xs =
+-- | This function excessively allocates closures every time 'f' is called. The
+-- closure in question
+bad :: [Int] -> Int
+bad xs = sum $ fmap f xs
+  where f x = x + length [1..10000]
 
--- | This function /does not/ create excessive closures. It returns a function
--- that has allocated the infinite list '[0..]' only once, then when the
--- function is called this list is simply referenced.
-good :: (Int -> a) -> (Int -> a)
-good f = (map f [0..] !!)
+good :: [Int] -> Int
+good xs = sum $ fmap f xs
+  where
+    n = length [1..10000]
+    f x = x + n
+
 
 
 main :: IO ()
 main = do
-  seed <- initStdGen
-  let test_values = take 5000 $ repeat 1
-  defaultMain [ bgroup "Too Many Closures" [ bench "bad"  $ whnf (fmap $ bad (+42)) test_values
-                                           , bench "good" $ whnf (fmap $ good (+42)) test_values
+  let test_values = replicate 5000 1
+  defaultMain [ bgroup "Too Many Closures" [ bench "bad"  $ whnf bad test_values
+                                           , bench "good" $ whnf good test_values
                                            ]
                    ]
