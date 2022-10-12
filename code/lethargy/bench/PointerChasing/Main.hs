df347b0fd2fae5e43a96e6dc3f8ec3c2dc67929d
doyougnu <jeffrey.young@iohk.io>
2022-10-12 08:20:17 -0400

    Eventlog: onto using markers section

--- code/lethargy/bench/PointerChasing/Main.hs
+++ code/lethargy/bench/PointerChasing/Main.hs
@@ -18,7 +18,7 @@
 {-# LANGUAGE BangPatterns #-}
 -- Need to disable optimizations because GHC will recognize and perform
 -- let-floating for us!
-{-# OPTIONS_GHC -O0 -ddump-simpl -ddump-to-file -ddump-stg-final #-}
+{-# OPTIONS_GHC -O2 -ddump-simpl -ddump-to-file -ddump-stg-final #-}
 
 module Main where
 
@@ -27,35 +27,43 @@ import System.Random          (mkStdGen)
 import System.Random.Stateful (newIOGenM, uniformRM)
 import Control.Concurrent     (threadDelay)
 import Control.Monad          (replicateM)
+import Control.DeepSeq        (force)
 
--- ys :: [Int]
--- ys = [1..10000]
+import Debug.Trace            (traceMarker, traceMarkerIO)
 
 lazy_mean :: [Double] -> Double
-lazy_mean xs = s / fromIntegral ln
+lazy_mean xs = traceMarker "Begin: lazy_mean" $ s / fromIntegral ln
   where (s, ln)        = foldl step (0,0) xs
         step (s, ln) a = (s + a, ln + 1)
 
 stricter_mean :: [Double] -> Double
-stricter_mean xs = s / fromIntegral ln
+stricter_mean xs = traceMarker "Begin: stricter_mean" $ s / fromIntegral (traceMarker "Done: stricter_mean" ln)
   where (s, ln)        = foldl' step (0,0) xs
         step (s, ln) a = (s + a, ln + 1)
 
 strict_mean :: [Double] -> Double
-strict_mean xs = s / fromIntegral ln
+strict_mean xs = traceMarker "Begin: strict_mean" $ s / fromIntegral ln
   where (s, ln)        = foldl' step (0,0) xs
         step (!s, !ln) a = (s + a, ln + 1)
 
 main :: IO ()
 main = do
+  let wait = threadDelay 100000
+  -- create a delay at the beginning of the program, if we don't do this then
+  -- our marker will be merged with the y-axis of the heap profile
+  wait
   -- generate random test data
+  traceMarkerIO "Bench Initialization"
   seed <- newIOGenM (mkStdGen 1729)
-  test_values <- replicateM 500000 $ uniformRM (0,500000) seed
-  -- sleep for a second
-  let wait = threadDelay 1000000
+  test_values <- fmap force $ replicateM 500000 $ uniformRM (0,500000) seed
+  traceMarkerIO "End Bench Initialization"
+  wait
   -- now run
   print $! lazy_mean test_values
+  traceMarkerIO "End lazy_mean"
   wait
   print $! stricter_mean test_values
+  traceMarkerIO "End stricter_mean"
   wait
   print $! strict_mean test_values
+  traceMarkerIO "End strict_mean"
