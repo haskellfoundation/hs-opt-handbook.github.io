269a5558698e8188d9c03055265c681b40f18deb
doyougnu <jmy6342@gmail.com>
2022-11-30 13:36:43 +0000

    Deploying to deploy from @ input-output-hk/hs-opt-handbook.github.io@04c967064607cbc50a1ca752f13f71ffb5bb635a 🚀

--- _static/lethargy/bench/Weigh/Main.hs
+++ _static/lethargy/bench/Weigh/Main.hs
@@ -31,34 +31,22 @@ import qualified Data.Map    as M
 import qualified Data.IntMap as IM
 import qualified Data.Set    as Set
 
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+
 import Weigh
 
-i :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int
-i a b c d e f g h i j k = a + b + c + d + e + f + h + i + j + k
+f,f',g_lifted :: Int -> Int -> Int
+f a 0 = a
+f a n = f (g (n `mod` 2)) (n - 1)
+  where g 0 = a
+        g n = 1 + g (n - 1)
 
-f :: Int -> Int -> Int -> Int -> Int -> Int -> Int
-f a b c d e f = a + b + c + d + e + f
+f' a 0 = a
+f' a n = f' (g_lifted a (n `mod` 2)) (n - 1)
 
-g :: Int -> Int -> Int -> Int -> Int -> Int
-g a b c d e = a + b + c + d + e
-
-count2 :: Int -> Int -> (Int,Int)
-count2 a b = (a,b)
-
-count3 :: Int -> Int -> (Int,Int,Int)
-count3 a b = (a,b,a)
-
-count4 :: Int -> Int -> (Int,Int,Int,Int)
-count4 a b = (a,b,a,b)
-
-count5 :: Int -> Int -> (Int,Int,Int,Int,Int)
-count5 a b = (a,b,a,b,a)
-
-count6 :: Int -> Int -> (Int,Int,Int,Int,Int,Int)
-count6 a b = (a,b,a,b,a,b)
-
-count9 :: Int -> Int -> (Int,Int,Int,Int,Int,Int,Int,Int,Int)
-count9 a b = (a,b,a,b,a,a,b,a,b)
+g_lifted a 0 = a
+g_lifted a n = 1 + g_lifted a (n - 1)
 
 list_o_tuples :: [Int] -> [Int] -> [(Int,Int)]
 list_o_tuples xs ys = [(x,y) | x <- xs, y <- ys]
@@ -72,22 +60,80 @@ data Foo1 = Foo1 Int
 data Foo2 = Foo2 String String
   deriving (Generic,NFData)
 
+data Foo3 = One Int
+          | Two Int Int
+          | TTT Int
+          | TTTT Int
+          | TTTTT Int
+          | TTTTTT Int
+          | TTTTTTT Int
+          | TTTTTTTT Int
+          | TTTTTTTTT Int
+          | TTTTTTTTTT Int
+          | TTTTTTTTTTT Int
+          | TTTTTTTTTTTT Int
+          | TTTTTTTTTTTTT Int
+          deriving (Generic,NFData)
+
+data SingleCons = SingleCons Int
+          deriving (Generic,NFData)
+
+data TwoConsReg = FirstCon Int
+                | SecondCon Int
+          deriving (Generic,NFData)
+
+data IrregularTwoCons = A Int
+                      | B Int
+                      | C Int
+                      | D Int
+                      | E Int
+                      | F Int
+                      | G Int
+                      | H Int
+                      | I Int
+                      | J Int
+                      | K Int
+          deriving (Generic,NFData)
+
 one,two :: String
 one = "one"
 two = "two"
 
+type Mytuple = (Int, Int)
+
+myTuple :: Mytuple
+myTuple = (3, 4)
+
+text_one :: T.Text
+text_one = T.pack one
+
+x,y :: Int
+
+x = f 100 10000
+y = f' 100 10000
+
 main :: IO ()
 main = mainWith $ do
-  value "()" ()
-  value "1"  (1 :: Int)
-  value "True"  True
-  value "[0..3]"  ([0..3] :: [Int])
-  value "[0,1,2,3]"  ([0,1,2,3] :: [Int])
-  value "Foo0"  Foo0
+  setColumns [Case, Allocated, Max, Live, GCs, MaxOS]
+  -- value "()" ()
+  -- value "1"  (1 :: Int)
+  -- value "True"  True
+  -- value "[0..3]"  ([0..3] :: [Int])
+  -- value "[0,1,2,3]"  ([0,1,2,3] :: [Int])
+  -- value "Foo0"  Foo0
   func  "Foo1-func"  Foo1 1
   value "Foo1-value"  (Foo1 1)
-  value "one" one
-  value "Foo2"  (Foo2 one two)
+  value "Foo3-value"  (  TTTTTTTTTTTTT 1)
+  value "SingleCons" (SingleCons 1)
+  value "TwoConsReg" (SecondCon 1000)
+  value "Irregular" (E 1)
+  -- value "one" one
+  -- value "Foo2"  (Foo2 one two)
+  value "myTuple" myTuple
+  func  "f"  (f 5) 10000
+  func  "f'"  (f 5) 10000
+  value  "fx"  x
+  value  "fy"  y
   -- value "([0,1,2],[3,4,5])"  ([[0..2], [3..5]] :: [[Int]])
   -- value "IntMap.Empty" (IM.empty :: IM.IntMap Int)
   -- value "Map.Empty"    (M.empty  :: M.Map Int Int)
@@ -102,3 +148,9 @@ main = mainWith $ do
   -- func "count6" (count6 1) 2
   -- func "count9" (count9 1) 2
   -- func "ListOTups" (list_o_tuples [0..1]) [2..3]
+  func "text strict" T.pack "very-long-text"
+  value "text lazy" (TL.pack "a")
+  value "text_string" "'"
+  value "text_one" text_one
+  value "string_one" one
+  value "a char" '.'
