4e17db8d3b5bec94e911b3630a98c4549a3b97d6
doyougnu <jmy6342@gmail.com>
2022-12-02 14:12:31 +0000

    Deploying to deploy from @ input-output-hk/hs-opt-handbook.github.io@a2de905f1c15039af3f199d3b7dffec325c6208a 🚀

--- _static/lethargy/bench/Weigh/Main.hs
+++ _static/lethargy/bench/Weigh/Main.hs
@@ -14,7 +14,8 @@
 --
 -- Tested with GHC 9.2.4
 -----------------------------------------------------------------------------
-{-# OPTIONS_GHC -O2 -ddump-simpl -ddump-to-file -ddump-stg-final #-}
+{-# OPTIONS_GHC -ddump-simpl -ddump-to-file -ddump-stg-final
+#-}
 
 module Main where
 
@@ -36,63 +37,70 @@ import qualified Data.Text.Lazy as TL
 
 import Weigh
 
-f,f',g_lifted :: Int -> Int -> Int
-f a 0 = a
-f a n = f (g (n `mod` 2)) (n - 1)
-  where g 0 = a
-        g n = 1 + g (n - 1)
+-- f,f',g_lifted :: Int -> Int -> Int
+-- f a 0 = a
+-- f a n = f (g (n `mod` 2)) (n - 1)
+--   where g 0 = a
+--         g n = 1 + g (n - 1)
 
-f' a 0 = a
-f' a n = f' (g_lifted a (n `mod` 2)) (n - 1)
+-- f' a 0 = a
+-- f' a n = f' (g_lifted a (n `mod` 2)) (n - 1)
 
-g_lifted a 0 = a
-g_lifted a n = 1 + g_lifted a (n - 1)
+-- g_lifted a 0 = a
+-- g_lifted a n = 1 + g_lifted a (n - 1)
 
-list_o_tuples :: [Int] -> [Int] -> [(Int,Int)]
-list_o_tuples xs ys = [(x,y) | x <- xs, y <- ys]
+-- list_o_tuples :: [Int] -> [Int] -> [(Int,Int)]
+-- list_o_tuples xs ys = [(x,y) | x <- xs, y <- ys]
 
-data Foo0 = Foo0
-  deriving (Generic,NFData)
+-- data Foo0 = Foo0
+--   deriving (Generic,NFData)
 
-data Foo1 = Foo1 Int
-  deriving (Generic,NFData)
+-- data Foo1 = Foo1 Int
+--   deriving (Generic,NFData)
 
 data Foo2 = Foo2 String String
   deriving (Generic,NFData)
 
-data Foo3 = One Int
-          | Two Int Int
-          | TTT Int
-          | TTTT Int
-          | TTTTT Int
-          | TTTTTT Int
-          | TTTTTTT Int
-          | TTTTTTTT Int
-          | TTTTTTTTT Int
-          | TTTTTTTTTT Int
-          | TTTTTTTTTTT Int
-          | TTTTTTTTTTTT Int
-          | TTTTTTTTTTTTT Int
-          deriving (Generic,NFData)
+-- data Foo3 = One Int
+--           | Two Int Int
+--           | TTT Int
+--           | TTTT Int
+--           | TTTTT Int
+--           | TTTTTT Int
+--           | TTTTTTT Int
+--           | TTTTTTTT Int
+--           | TTTTTTTTT Int
+--           | TTTTTTTTTT Int
+--           | TTTTTTTTTTT Int
+--           | TTTTTTTTTTTT Int
+--           | TTTTTTTTTTTTT Int
+--           deriving (Generic,NFData)
 
-data SingleCons = SingleCons Int
-          deriving (Generic,NFData)
+-- data SingleCons = SingleCons Int
+--           deriving (Generic,NFData)
 
 data TwoConsReg = FirstCon Int
                 | SecondCon Int
           deriving (Generic,NFData)
 
-data IrregularTwoCons = A Int
-                      | B Int
-                      | C Int
-                      | D Int
-                      | E Int
-                      | F Int
-                      | G Int
-                      | H Int
-                      | I Int
-                      | J Int
-                      | K Int
+data LotsOfInts  = A Int Int
+                 | B Int Int
+                 | C Int Int
+                 | D Int Int
+                 | E Int Int
+          deriving (Generic,NFData)
+
+data LotsOfInts2 = A2 Int Int
+                 | B2 Int Int
+                 | C2 Int Int
+                 | D2 Int Int
+                 | E2 Int Int
+                 | F2 Int Int
+                 | G2 Int Int
+                 | H2 Int Int
+                 | I2 Int Int
+                 | J2 Int Int
+                 | K2 Int Int
           deriving (Generic,NFData)
 
 one,two :: String
@@ -104,36 +112,38 @@ type Mytuple = (Int, Int)
 myTuple :: Mytuple
 myTuple = (3, 4)
 
-text_one :: T.Text
-text_one = T.pack one
+-- text_one :: T.Text
+-- text_one = T.pack one
 
-x,y :: Int
+-- x,y :: Int
 
-x = f 100 10000
-y = f' 100 10000
+-- x = f 100 10000
+-- y = f' 100 10000
 
-main :: IO ()
-main = mainWith $ do
-  setColumns [Case, Allocated, Max, Live, GCs, MaxOS]
   -- value "()" ()
   -- value "1"  (1 :: Int)
   -- value "True"  True
   -- value "[0..3]"  ([0..3] :: [Int])
   -- value "[0,1,2,3]"  ([0,1,2,3] :: [Int])
   -- value "Foo0"  Foo0
-  func  "Foo1-func"  Foo1 1
-  value "Foo1-value"  (Foo1 1)
-  value "Foo3-value"  (  TTTTTTTTTTTTT 1)
-  value "SingleCons" (SingleCons 1)
-  value "TwoConsReg" (SecondCon 1000)
-  value "Irregular" (E 1)
-  -- value "one" one
-  -- value "Foo2"  (Foo2 one two)
+  -- func  "Foo1-func"  Foo1 1
+  -- value "Foo1-value"  (Foo1 1)
+  -- value "Foo3-value"  (  TTTTTTTTTTTTT 1)
+main :: IO ()
+main = mainWith $ do
+  setColumns [Case, Allocated, Max, Live, GCs, MaxOS]
+  -- value "SingleCons" (SingleCons 1729)
+  value "A Lot Of Ints" (A 1000 1001)
+  func "A Lot Of Ints" (A 1000) 1001
+  -- value "Irregular" (E 1)
+  -- value "1" (1 :: Int)
+  value "Foo2"  (Foo2 one two)
   value "myTuple" myTuple
-  func  "f"  (f 5) 10000
-  func  "f'"  (f 5) 10000
-  value  "fx"  x
-  value  "fy"  y
+  value "one" one
+  -- func  "f"  (f 5) 10000
+  -- func  "f'"  (f 5) 10000
+  -- value  "fx"  x
+  -- value  "fy"  y
   -- value "([0,1,2],[3,4,5])"  ([[0..2], [3..5]] :: [[Int]])
   -- value "IntMap.Empty" (IM.empty :: IM.IntMap Int)
   -- value "Map.Empty"    (M.empty  :: M.Map Int Int)
@@ -148,9 +158,9 @@ main = mainWith $ do
   -- func "count6" (count6 1) 2
   -- func "count9" (count9 1) 2
   -- func "ListOTups" (list_o_tuples [0..1]) [2..3]
-  func "text strict" T.pack "very-long-text"
-  value "text lazy" (TL.pack "a")
-  value "text_string" "'"
-  value "text_one" text_one
-  value "string_one" one
-  value "a char" '.'
+  -- func "text strict" T.pack "very-long-text"
+  -- value "text lazy" (TL.pack "a")
+  -- value "text_string" "'"
+  -- value "text_one" text_one
+  -- value "string_one" one
+  -- value "a char" '.'
